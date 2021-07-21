cd29f3381a7081787623d5eb0af1fdb61448c930
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-07-21 03:56:53 +0200

    Remove footable menu item. Correct installerScript

--- installerScript.php
+++ installerScript.php
@@ -7,43 +7,63 @@ use Joomla\CMS\Log\Log;
 
 class bs4ghsvsInstallerScript extends InstallerScript
 {
+	/**
+	 * A list of files to be deleted with method removeFiles().
+	 *
+	 * @var    array
+	 * @since  2.0
+	 */
+	protected $deleteFiles = [
+		'/templates/bs4ghsvs/html/com_content/category/blogghsvs-bs_footableghsvs.php'
+	];
+
+	/**
+	 * A list of folders to be deleted with method removeFiles().
+	 *
+	 * @var    array
+	 * @since  2.0
+	 */
+
+	protected $deleteFolders = [
+		'/templates/bs4ghsvs/html/com_tags'
+	];
+
 	public function preflight($type, $parent)
 	{
-		if (!parent::preflight($type, $parent))
-		{
-			return false;
-		}
-
 		$manifest = @$parent->getManifest();
 
 		if ($manifest instanceof SimpleXMLElement)
 		{
-			$minimumPhp = trim((string) $manifest->minimumPhp);
-			$minimumJoomla = trim((string) $manifest->minimumJoomla);
-
-			// Custom
-			$maximumPhp = trim((string) $manifest->maximumPhp);
-			$maximumJoomla = trim((string) $manifest->maximumJoomla);
-
-			$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
-			$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
-
-			if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+			if ($type === 'update' || $type === 'install' || $type === 'discover_install')
 			{
-				$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
-				Log::add($msg, Log::WARNING, 'jerror');
-			}
+				$minimumPhp = trim((string) $manifest->minimumPhp);
+				$minimumJoomla = trim((string) $manifest->minimumJoomla);
 
-			// Check for the maximum PHP version before continuing
-			if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
-			{
-				$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
-				Log::add($msg, Log::WARNING, 'jerror');
-			}
+				// Custom
+				$maximumPhp = trim((string) $manifest->maximumPhp);
+				$maximumJoomla = trim((string) $manifest->maximumJoomla);
 
-			if (isset($msg))
-			{
-				return false;
+				$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
+				$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
+
+				if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+				{
+					$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
+					Log::add($msg, Log::WARNING, 'jerror');
+				}
+
+				// Check for the maximum PHP version before continuing
+				if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
+				{
+					$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
+
+					Log::add($msg, Log::WARNING, 'jerror');
+				}
+
+				if (isset($msg))
+				{
+					return false;
+				}
 			}
 
 			if (trim((string) $manifest->allowDowngrades))
@@ -51,6 +71,36 @@ class bs4ghsvsInstallerScript extends InstallerScript
 				$this->allowDowngrades = true;
 			}
 		}
+
+		if (!parent::preflight($type, $parent))
+		{
+			return false;
+		}
+
+		if ($type === 'update')
+		{
+			//$this->removeOldUpdateservers();
+		}
+
 		return true;
+	}
+
+	/**
+	 * Runs right after any installation action is preformed on the component.
+	 *
+	 * @param  string    $type   - Type of PostFlight action. Possible values are:
+	 *                           - * install
+	 *                           - * update
+	 *                           - * discover_install
+	 * @param  \stdClass $parent - Parent object calling object.
+	 *
+	 * @return void
+	 */
+	function postflight($type, $parent)
+	{
+		if ($type === 'update')
+		{
+			$this->removeFiles();
+		}
 	}
 }
