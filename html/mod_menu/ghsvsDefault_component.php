28ae898817b9461aaafdba18fbc2a0f5eade135a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-07-24 23:37:58 +0200

    Reduce code

--- html/mod_menu/ghsvsDefault_component.php
+++ html/mod_menu/ghsvsDefault_component.php
@@ -4,12 +4,10 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Filter\OutputFilter;
 
-$attributes = $item->aAttributes;
-
 echo HTMLHelper::_('link',
 	OutputFilter::ampReplace(htmlspecialchars(
 		$item->flink, ENT_COMPAT, 'UTF-8', false)
 	),
 	$item->title,
-	$attributes
+	$item->aAttributes
 );
