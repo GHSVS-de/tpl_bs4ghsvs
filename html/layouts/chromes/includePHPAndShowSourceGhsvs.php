4bf6e33ccb30b569652b6a79958d901616243bd6
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-07-28 03:07:52 +0200

    Move first module style to JLayout ...
    modChrome_includePHPAndShowSourceGhsvs

---
+++ html/layouts/chromes/includePHPAndShowSourceGhsvs.php
@@ -0,0 +1,69 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+$module = $displayData['module'];
+$params = $displayData['params'];
+$attribs = $displayData['attribs'];
+
+$includeFile = JPATH_BASE . '/media/php-includes_ghsvs/' . $module->position
+	. '.php';
+
+if (!is_file($includeFile))
+{
+	return;
+}
+
+if ($spoilerOn = $params->get('spoiler', 1))
+{
+	// $spoiler_in = $params->get('spoiler_in', 0);
+	JLoader::register('Bs3ghsvsPagebreak',
+		JPATH_PLUGINS . '/system/bs3ghsvs/Helper/PagebreakHelper.php');
+	$options = [
+		'activeToSession' => 0
+	];
+}
+
+if ((bool) $module->showtitle)
+{
+	$headerTag = htmlspecialchars($params->get('header_tag', 'h3'));
+	$headerClass = $params->get('header_class');
+	$headerClass = !empty($headerClass) ? ' class="'
+		. htmlspecialchars($headerClass) . '"' : '';
+	echo '<' . $headerTag . $headerClass .'>' . $module->title
+		. '</' . $headerTag . '>';
+}
+
+ob_start();
+require_once($includeFile);
+$text_1 = ob_get_clean();
+
+$includeFile_ = basename($includeFile);
+
+$text_2 = '<pre class="language-php line-numbers modChrome_includePHPAndSourceGhsvs"><code>'
+	. htmlentities(file_get_contents($includeFile), ENT_QUOTES, 'UTF-8')
+	. '</code></pre>';
+
+HTMLHelper::_('content.prepare', $text_2, '', 'com_content.article');
+
+if ($spoilerOn)
+{
+	$text = '<p>{pagebreakghsvs-slider title="'
+		. Text::sprintf('PLG_SYSTEM_BS3GHSVS_SHOW_HIDE_OUTPUT_OF_FILE', $includeFile_) . '" title2=""}</p>'
+		. $text_1
+		. '';
+
+	$text .= '{pagebreakghsvs-slider title="'
+		. Text::sprintf('GHSVS_MODULES_SPOILER_BTN_TEXT', $includeFile_)
+		. '" title2=""}' . $text_2;
+
+		Bs3ghsvsPagebreak::buildSliders($text, $module->id . '-sp1', $options);
+}
+else
+{
+	$text = $text_1 . $text_2;
+}
+
+echo $text;
--- html/modules.php
+++ html/modules.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
 
 /*
  * Module chrome for rendering the module in a submenu
@@ -108,129 +109,11 @@ Z.B. für /media/php-includes_ghsvs/icomoonclasses.php
 Siehe /media/php-includes_ghsvs/ReadMe.txt
 Dieser Stil zeigt zusätzlich den PHP-Quellcode der ausgeführten Datei an.
 */
-function modChrome_includePHPAndShowSourceGhsvssssssssss($module, &$params, &$attribs)
-{
-	$includeFile = JPATH_BASE . '/media/php-includes_ghsvs/' . $module->position . '.php';
-
-	if (!is_file($includeFile))
-	{
-		return;
-	}
-
-	$spoilerOn = $params->get('spoiler', 1);
-
-	if ((bool) $module->showtitle)
-	{
-		$headerTag = htmlspecialchars($params->get('header_tag', 'h3'));
-		$headerClass = $params->get('header_class');
-		$headerClass = !empty($headerClass) ? ' class="' . htmlspecialchars($headerClass) . '"' : '';
-		echo '<' . $headerTag . $headerClass .'>' . $module->title .'</' . $headerTag . '>';
-	}
-
-	ob_start();
-	require_once($includeFile);
-	$text = ob_get_clean();
-
-	$includeFile_ = basename($includeFile);
-
-	if ($spoilerOn)
-	{
-		echo HTMLHelper::_('bs3ghsvs.spoiler',
-			$text,
-			array(
-				'buttontext' => Text::sprintf('PLG_SYSTEM_BS3GHSVS_SHOW_HIDE_OUTPUT_OF_FILE', $includeFile_),
-				'in' => $params->get('spoiler_in', 0),
-				// 'spoilerclass' => '',
-				'buttonclass' => 'btn btn-catcolor',
-				// 'role' => 'navigation',
-			)
-		);
-	}
-	else
-	{
-		echo $text;
-	}
-
-	$text = '<pre class="modChrome_includePHPAndSourceGhsvs"><code>'
-		. htmlentities(file_get_contents($includeFile), ENT_QUOTES, 'UTF-8') . '</code></pre>';
-
-	$text .= Text::_('GHSVS_MODULES_SCRIPT_HINT');
-
-	echo HTMLHelper::_('bs3ghsvs.spoiler',
-		$text,
-		array(
-			'buttontext' => Text::sprintf('GHSVS_MODULES_SPOILER_BTN_TEXT', $includeFile_),
-			'in' => $params->get('spoiler_in', 0),
-			// 'spoilerclass' => '',
-			'buttonclass' => 'btn btn-catcolor',
-			// 'role' => 'navigation',
-		)
-	);
-}
-
-
-
-/*
-2015-08-30
-Z.B. für /media/php-includes_ghsvs/icomoonclasses.php
-Siehe /media/php-includes_ghsvs/ReadMe.txt
-Dieser Stil zeigt zusätzlich den PHP-Quellcode der ausgeführten Datei an.
-*/
 function modChrome_includePHPAndShowSourceGhsvs($module, &$params, &$attribs)
 {
-	$includeFile = JPATH_BASE . '/media/php-includes_ghsvs/' . $module->position . '.php';
-
-	if (!is_file($includeFile))
-	{
-		return;
-	}
-
-	if ($spoilerOn = $params->get('spoiler', 1))
-	{
-		// $spoiler_in = $params->get('spoiler_in', 0);
-		JLoader::register('Bs3ghsvsPagebreak',
-			JPATH_PLUGINS . '/system/bs3ghsvs/Helper/PagebreakHelper.php');
-		$options = [
-			'activeToSession' => 0
-		];
-	}
-
-	if ((bool) $module->showtitle)
-	{
-		$headerTag = htmlspecialchars($params->get('header_tag', 'h3'));
-		$headerClass = $params->get('header_class');
-		$headerClass = !empty($headerClass) ? ' class="' . htmlspecialchars($headerClass) . '"' : '';
-		echo '<' . $headerTag . $headerClass .'>' . $module->title .'</' . $headerTag . '>';
-	}
-
-	ob_start();
-	require_once($includeFile);
-	$text_1 = ob_get_clean();
-
-	$includeFile_ = basename($includeFile);
-
-	$text_2 = '<pre class="language-php line-numbers modChrome_includePHPAndSourceGhsvs"><code>'
-		. htmlentities(file_get_contents($includeFile), ENT_QUOTES, 'UTF-8') . '</code></pre>';
-
-	HTMLHelper::_('content.prepare', $text_2, '', 'com_content.article');
-
-	if ($spoilerOn)
-	{
-		$text = '<p>{pagebreakghsvs-slider title="'
-			. Text::sprintf('PLG_SYSTEM_BS3GHSVS_SHOW_HIDE_OUTPUT_OF_FILE', $includeFile_) . '" title2=""}</p>'
-			. $text_1
-			. '';
-
-		$text .= '{pagebreakghsvs-slider title="'
-			. Text::sprintf('GHSVS_MODULES_SPOILER_BTN_TEXT', $includeFile_) . '" title2=""}'
-			. $text_2;
-
-			Bs3ghsvsPagebreak::buildSliders($text, $module->id . '-sp1', $options);
-	}
-	else
-	{
-		$text = $text_1 . $text_2;
-	}
-
-	echo $text;
+	echo LayoutHelper::render('chromes.includePHPAndShowSourceGhsvs',
+	['module' => $module,
+	'params' => $params,
+	'attribs' => $attribs]
+	);
 }
