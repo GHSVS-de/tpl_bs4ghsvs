7e3e5cf5f886019c8cce0b9297741fa8b7ed28d9
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-08-14 22:51:38 +0200

    print.php. Fix J!4 bug. $this->base
    - $this->base doesn't exist anymore for current URL.
    - Other smaller, "just to go sure", changes

--- src/print.php
+++ src/print.php
@@ -1,11 +1,13 @@
 <?php
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Uri\Uri;
+
 $this->_scripts = $this->_styleSheets = $this->_script = $this->_style = $this->_script = $this->_custom = $this->_scriptText = array();
 $this->link = '';
 $this->setMetadata('robots', 'noindex,nofollow');
-$this->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template.min.css');
-$this->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/print.min.css');
+$this->addStyleSheet(Uri::root() . 'templates/' . $this->template . '/css/template.min.css');
+$this->addStyleSheet(Uri::root() . 'templates/' . $this->template . '/css/print.min.css');
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
@@ -15,8 +17,8 @@ $this->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/pr
 </head>
 <body class="bodyPrint ausdruck">
  <p class="printintro text-monospace">
-  - <?php echo JUri::getInstance()->getHost() ?>, <?php echo date('Y-m-d'); ?><br />
-  - <?php echo $this->base; ?><br />
+  - <?php echo Uri::root(); ?>, <?php echo date('Y-m-d'); ?><br />
+  - <?php echo Uri::current(); ?><br />
   - <?php echo $this->title; ?>
  </p>
 	<jdoc:include type="component" />
