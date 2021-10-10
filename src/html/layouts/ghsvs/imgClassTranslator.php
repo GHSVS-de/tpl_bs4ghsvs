f5f32344c56f5033a141e1cb87b5515f276b7018
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-10-11 00:27:55 +0200

    Update imgClassTranslator.php
    - Just description. Not important

--- src/html/layouts/ghsvs/imgClassTranslator.php
+++ src/html/layouts/ghsvs/imgClassTranslator.php
@@ -1,11 +1,25 @@
 <?php
-// Ist kein JLayout. Via require einzubinden.
-// Hard overrides for old Joomla float classes.
-// figureClass Kann man im Beitrag einstellen, falls articleImages.xml im bs3ghsvs-
-// Plugin geladen wird, was z.B. float_intro-Feld mit CSS-Klassen versieht.
-// Dieses Array hier benötigen wir, falls nur normale Joomla3-Einstellungen
-// möglich sind bzw. bei ungespeicherten Beiträgen auf die Globalen Optionen
-// zurückfallen.
+/*
+Für Joomla 3 gedacht. Joomla 4 weiß ich noch nicht, ob da auch 'right', 'left',
+'none' die Joomlaeinstellungen sein werden.
+
+Ist kein JLayout. Via require einzubinden. z.B. siehe JLayout full_image_venobox.php.
+
+require(__DIR__ . '/imgClassTranslator.php');
+$figureClass = $images->get('float_fulltext', 'ghsvs_img-right');
+
+if (!empty($imgClassTranslator[$figureClass]))
+{
+	$figureClass = $imgClassTranslator[$figureClass];
+}
+
+Wenn das Plugin bs3ghsvs und articleImages.xml geladen wird, ist eine Eingabe
+des floats nicht möglich im Beitrag.
+
+Man steuert über die Globalen Inhalts-Optionen.
+
+Benötigt dann natürlich auch entprechednes SCSS für meine Klassen unten.
+*/
 
 $imgClassTranslator = [
     'right' => 'ghsvs_img-default',
